1479f885acf818fc857b7c40ad0ce453a87a92dc
TwilightLoL <info@team-xte.com>
2014-10-30 20:52:29 +0200

    HP Bar damage drawing

--- TwilightRyze/Program.cs
+++ TwilightRyze/Program.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using Color = System.Drawing.Color;
 using System.Linq;
 using SharpDX;
+using SharpDX.Direct3D9;
 using LeagueSharp;
 using LeagueSharp.Common;
 using LX_Orbwalker;
+using System.Globalization;
 using System.Threading;
 #endregion
 
@@ -26,6 +28,7 @@ namespace LightningRyze
 		private static Spell E;
 		private static Spell R;
 		private static SpellSlot IgniteSlot;
+        public static HpBarIndicator hpi = new HpBarIndicator();
 		
         private static void Main(string[] args)
         {
@@ -552,5 +555,144 @@ namespace LightningRyze
         		count = count + 1 ;
             return count;
         }
+
+
+        private static void OnEndScene(EventArgs args)
+        {
+            if (Config.Item("drawDamage").GetValue<bool>())
+            {
+                foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(ene => !ene.IsDead && ene.IsEnemy && ene.IsVisible))
+                {
+                        hpi.unit = enemy;
+                        hpi.drawDmg(GetComboDamage(enemy), Color.Yellow);
+                }
+            }
+        }
+
+        private static void OnDraw(EventArgs args)
+        {
+            try
+            {
+                if(Config.Item("drawDamage").GetValue<bool>())
+                {
+                    foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(ene => !ene.IsDead && ene.IsEnemy && ene.IsVisible))
+                    {
+                        hpi.unit = enemy;
+                        hpi.drawDmg(GetComboDamage(enemy), Color.Yellow);
+                    }
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+
+
+    }
+    class HpBarIndicator
+    {
+
+        public static SharpDX.Direct3D9.Device dxDevice = Drawing.Direct3DDevice;
+        public static SharpDX.Direct3D9.Line dxLine;
+
+        public Obj_AI_Hero unit { get; set; }
+
+        public float width = 104;
+
+        public float hight = 9;
+
+
+        public HpBarIndicator()
+        {
+            dxLine = new Line(dxDevice) { Width = 9 };
+
+            Drawing.OnPreReset += DrawingOnOnPreReset;
+            Drawing.OnPostReset += DrawingOnOnPostReset;
+            AppDomain.CurrentDomain.DomainUnload += CurrentDomainOnDomainUnload;
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomainOnDomainUnload;
+
+        }
+
+
+        private static void CurrentDomainOnDomainUnload(object sender, EventArgs eventArgs)
+        {
+            dxLine.Dispose();
+        }
+
+        private static void DrawingOnOnPostReset(EventArgs args)
+        {
+            dxLine.OnResetDevice();
+        }
+
+        private static void DrawingOnOnPreReset(EventArgs args)
+        {
+            dxLine.OnLostDevice();
+        }
+
+        private Vector2 Offset
+        {
+            get
+            {
+                if (unit != null)
+                {
+                    return unit.IsAlly ? new Vector2(34, 9) : new Vector2(10, 20);
+                }
+
+                return new Vector2();
+            }
+        }
+
+        public Vector2 startPosition
+        {
+
+            get { return new Vector2(unit.HPBarPosition.X + Offset.X, unit.HPBarPosition.Y + Offset.Y); }
+        }
+
+
+        private float getHpProc(float dmg = 0)
+        {
+            float health = ((unit.Health - dmg) > 0) ? (unit.Health - dmg) : 0;
+            return (health / unit.MaxHealth);
+        }
+
+        private Vector2 getHpPosAfterDmg(float dmg)
+        {
+            float w = getHpProc(dmg) * width;
+            return new Vector2(startPosition.X + w, startPosition.Y);
+        }
+
+        public void drawDmg(float dmg, System.Drawing.Color color)
+        {
+            var hpPosNow = getHpPosAfterDmg(0);
+            var hpPosAfter = getHpPosAfterDmg(dmg);
+
+            fillHPBar(hpPosNow, hpPosAfter, color);
+            //fillHPBar((int)(hpPosNow.X - startPosition.X), (int)(hpPosAfter.X- startPosition.X), color);
+        }
+
+        private void fillHPBar(int to, int from, System.Drawing.Color color)
+        {
+            Vector2 sPos = startPosition;
+
+            for (int i = from; i < to; i++)
+            {
+                Drawing.DrawLine(sPos.X + i, sPos.Y, sPos.X + i, sPos.Y + 9, 1, color);
+            }
+        }
+
+        private void fillHPBar(Vector2 from, Vector2 to, System.Drawing.Color color)
+        {
+            dxLine.Begin();
+
+            dxLine.Draw(new[]
+                                    {
+                                        new Vector2((int)from.X, (int)from.Y + 4f),
+                                        new Vector2( (int)to.X, (int)to.Y + 4f)
+                                    }, new ColorBGRA(255, 255, 00, 90));
+            // Vector2 sPos = startPosition;
+            //Drawing.DrawLine((int)from.X, (int)from.Y + 9f, (int)to.X, (int)to.Y + 9f, 9f, color);
+
+            dxLine.End();
+        }
+
     }
 }
