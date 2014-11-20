9a5dddbea6965415670002c5065db0b7b7969c14
TwilightLoL <info@team-xte.com>
2014-11-20 13:13:54 +0200

    TAC: Kalista

---
+++ Twilight's Auto Carry - Kalista/Twilight's Auto Carry - Kalista/Program.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using LX_Orbwalker;
+using SharpDX;
+using SharpDX.Direct3D9;
+using Color = System.Drawing.Color;
+using System.Globalization;
+using System.Threading;
+
+namespace Twilight_s_Auto_Carry___Kalista
+{
+    class Program
+    {
+        public static HpBarIndicator hpi = new HpBarIndicator();
+        private static Menu Config;
+        private static Obj_AI_Hero target;
+        private static Obj_AI_Hero myHero;
+        private static Spell Q = new Spell(SpellSlot.Q,1450);
+        private static Spell W = new Spell(SpellSlot.W,5500);
+        private static Spell E = new Spell(SpellSlot.E,1050);
+        private static Spell R = new Spell(SpellSlot.R,1200);
+        
+        static void Main(string[] args)
+        {
+            if (myHero.ChampionName != "Kalista")
+                return;
+            Game.PrintChat("=========================");
+            Game.PrintChat("| Twilight Auto Carry   |");
+            Game.PrintChat("=========================");
+            Game.PrintChat("Loading kalista plugin!");
+            CustomEvents.Game.OnGameLoad += Load;
+            Game.PrintChat("Kalista loaded!");
+            Game.PrintChat("=========================");
+        }
+        public static void Load(EventArgs args)
+        {
+            Config = new Menu("TAC: Kalista", "Kalista", true);
+            var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
+            SimpleTs.AddToMenu(targetSelectorMenu);
+            Config.AddSubMenu(targetSelectorMenu);
+
+            var orbwalkerMenu = new Menu("LX-Orbwalker", "Orbwalker");
+            LXOrbwalker.AddToMenu(orbwalkerMenu);
+            Config.AddSubMenu(orbwalkerMenu);
+
+            Config.AddSubMenu(new Menu("Drawings", "Drawings"));
+            Config.SubMenu("Drawings").AddItem(new MenuItem("QRange", "Q range").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
+            Config.SubMenu("Drawings").AddItem(new MenuItem("WRange", "W range").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
+            Config.SubMenu("Drawings").AddItem(new MenuItem("ERange", "E range").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
+            Config.SubMenu("Drawings").AddItem(new MenuItem("RRange", "R range").SetValue(new Circle(true, Color.FromArgb(255, 255, 255, 255))));
+            Config.AddToMainMenu();
+
+            Config.AddItem(new MenuItem("debug", "Debug").SetValue(true));
+
+
+            Drawing.OnDraw += Drawing_OnDraw;
+            Drawing.OnEndScene += OnEndScene;
+        }
+
+        private static void OnEndScene(EventArgs args)
+        {
+            if (Config.Item("drawDamage").GetValue<bool>())
+            {
+                foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(ene => !ene.IsDead && ene.IsEnemy && ene.IsVisible))
+                {
+                    hpi.unit = enemy;
+                    hpi.drawDmg(CalculateRendDamage(enemy), Color.Yellow);
+                }
+            }
+        }
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+
+            var drawQ = Config.Item("QRange").GetValue<Circle>();
+            if (drawQ.Active && !myHero.IsDead)
+            {
+                Utility.DrawCircle(myHero.Position, Q.Range, drawQ.Color);
+            }
+
+            var drawW = Config.Item("WRange").GetValue<Circle>();
+            if (drawW.Active && !myHero.IsDead)
+            {
+                Utility.DrawCircle(myHero.Position, W.Range, drawW.Color);
+            }
+            var drawE = Config.Item("ERange").GetValue<Circle>();
+            if (drawE.Active && !myHero.IsDead)
+            {
+                Utility.DrawCircle(myHero.Position, E.Range, drawE.Color);
+            }
+
+            var drawR = Config.Item("RRange").GetValue<Circle>();
+            if (drawR.Active && !myHero.IsDead)
+            {
+                Utility.DrawCircle(myHero.Position, R.Range, drawR.Color);
+            }
+            try
+            {
+                foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(ene => !ene.IsDead && ene.IsEnemy && ene.IsVisible))
+                {
+                    hpi.unit = enemy;
+                    if (CalculateRendDamage(enemy) >= enemy.Health)
+                    {
+                        hpi.drawDmg(CalculateRendDamage(enemy), Color.Red);
+                    }
+                    else
+                    {
+                        hpi.drawDmg(CalculateRendDamage(enemy), Color.Yellow);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Game.PrintChat("Failed to draw HP bar damage! => " + ex);
+            }
+        }
+
+        public static float CalculateRendDamage(Obj_AI_Hero eTarget)
+        {
+            if (E.IsReady())
+            {
+//                var eTarget = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
+                if (eTarget.IsValidTarget(E.Range))
+                {
+                    foreach (var buff in eTarget.Buffs.Where(buff => buff.DisplayName.ToLower() == "kalistarend").Where(buff => buff.Count == 6))
+                    {
+                        Game.PrintChat("Total stacks on target " + eTarget.ChampionName + " Count: " + buff.Count + " Total Damage: " + eTarget.GetSpellDamage(myHero,SpellSlot.E)*buff.Count);
+                        return (float)eTarget.GetSpellDamage(myHero, SpellSlot.E) * buff.Count;
+//                        E.Cast();
+                    }
+                }
+
+            }
+            return (float)0;
+        }
+    }
+    class HpBarIndicator
+    {
+
+        public static SharpDX.Direct3D9.Device dxDevice = Drawing.Direct3DDevice;
+        public static SharpDX.Direct3D9.Line dxLine;
+
+        public Obj_AI_Hero unit { get; set; }
+
+        public float width = 104;
+
+        public float hight = 9;
+
+
+        public HpBarIndicator()
+        {
+            dxLine = new Line(dxDevice) { Width = 9 };
+
+            Drawing.OnPreReset += DrawingOnOnPreReset;
+            Drawing.OnPostReset += DrawingOnOnPostReset;
+            AppDomain.CurrentDomain.DomainUnload += CurrentDomainOnDomainUnload;
+            AppDomain.CurrentDomain.ProcessExit += CurrentDomainOnDomainUnload;
+
+        }
+
+
+        private static void CurrentDomainOnDomainUnload(object sender, EventArgs eventArgs)
+        {
+            dxLine.Dispose();
+        }
+
+        private static void DrawingOnOnPostReset(EventArgs args)
+        {
+            dxLine.OnResetDevice();
+        }
+
+        private static void DrawingOnOnPreReset(EventArgs args)
+        {
+            dxLine.OnLostDevice();
+        }
+
+        private Vector2 Offset
+        {
+            get
+            {
+                if (unit != null)
+                {
+                    return unit.IsAlly ? new Vector2(34, 9) : new Vector2(10, 20);
+                }
+
+                return new Vector2();
+            }
+        }
+
+        public Vector2 startPosition
+        {
+
+            get { return new Vector2(unit.HPBarPosition.X + Offset.X, unit.HPBarPosition.Y + Offset.Y); }
+        }
+
+
+        private float getHpProc(float dmg = 0)
+        {
+            float health = ((unit.Health - dmg) > 0) ? (unit.Health - dmg) : 0;
+            return (health / unit.MaxHealth);
+        }
+
+        private Vector2 getHpPosAfterDmg(float dmg)
+        {
+            float w = getHpProc(dmg) * width;
+            return new Vector2(startPosition.X + w, startPosition.Y);
+        }
+
+        public void drawDmg(float dmg, System.Drawing.Color color)
+        {
+            var hpPosNow = getHpPosAfterDmg(0);
+            var hpPosAfter = getHpPosAfterDmg(dmg);
+
+            fillHPBar(hpPosNow, hpPosAfter, color);
+            //fillHPBar((int)(hpPosNow.X - startPosition.X), (int)(hpPosAfter.X- startPosition.X), color);
+        }
+
+        private void fillHPBar(int to, int from, System.Drawing.Color color)
+        {
+            Vector2 sPos = startPosition;
+
+            for (int i = from; i < to; i++)
+            {
+                Drawing.DrawLine(sPos.X + i, sPos.Y, sPos.X + i, sPos.Y + 9, 1, color);
+            }
+        }
+
+        private void fillHPBar(Vector2 from, Vector2 to, System.Drawing.Color color)
+        {
+            dxLine.Begin();
+
+            dxLine.Draw(new[]
+                                    {
+                                        new Vector2((int)from.X, (int)from.Y + 4f),
+                                        new Vector2( (int)to.X, (int)to.Y + 4f)
+                                    }, new ColorBGRA(255, 255, 00, 90));
+            // Vector2 sPos = startPosition;
+            //Drawing.DrawLine((int)from.X, (int)from.Y + 9f, (int)to.X, (int)to.Y + 9f, 9f, color);
+
+            dxLine.End();
+        }
+
+    }
+}
