19bd916fb407dcf661c88b4fc8d0ad40bfe0bab4
TwilightLoL <info@team-xte.com>
2014-11-26 21:34:19 +0200

    minor tweak

--- TAC_Kalista/TAC Kalista/MathHandler.cs
+++ TAC_Kalista/TAC Kalista/MathHandler.cs
@@ -53,6 +53,7 @@ namespace TAC_Kalista
          * dealing 20/30/40/50/60 (+0.6) physical damage 
          * and slowing their Movement Speed by 25/30/35/40/45% for 2 seconds. 
          * >>Each extra spear increases the damage by 25/30/35/40/45%<<
+         * >>Damage per Additional Spear: 5 / 9 / 14 / 20 / 27 (+15 / 18 / 21 / 24 / 27% AD) <<
          * , but not the slow.
          * http://ddragon.leagueoflegends.com/tool/euw/en
          * Apperantly hellsing sucks at math, so i had to do this code on my own.
@@ -62,13 +63,19 @@ namespace TAC_Kalista
             int skillLevel = SkillHandler.E.Level;
             int stacks = target.Buffs.FirstOrDefault(b => b.DisplayName == "KalistaExpungeMarker").Count;
             double basicDamagex = new double[] { 0, 20, 30, 40, 50, 60 }[skillLevel];
-            double basicDamage = new double[] { 0, 20, 30, 40, 50, 60 }[skillLevel] + (0.6 * (ObjectManager.Player.BaseAttackDamage + ObjectManager.Player.FlatPhysicalDamageMod));
-            double extraDamage = new double[] { 0, 0.25, 0.30, 0.35, 0.40, 0.45 }[skillLevel];
+            //double extraDamage = new double[] { 0, 0.25, 0.30, 0.35, 0.40, 0.45 }[skillLevel];
 
-            double realDamage = ObjectManager.Player.CalcDamage(target, Damage.DamageType.Physical, stacks > 1 ? basicDamage + (basicDamage * extraDamage)*(stacks-1) : basicDamage);
+
+            double basicDamage = new double[] { 0, 20, 30, 40, 50, 60 }[skillLevel] + (0.6 * (ObjectManager.Player.BaseAttackDamage + ObjectManager.Player.FlatPhysicalDamageMod));
+            double addidentionalDamage = new double[] { 0, 5, 9, 14, 20, 27 }[skillLevel] + (new double[] { 0, 0.15, 0.18, 0.21, 0.24, 0.27 }[skillLevel] * (ObjectManager.Player.BaseAttackDamage + ObjectManager.Player.FlatPhysicalDamageMod));
+
+
+            double realDamage = ObjectManager.Player.CalcDamage(target, Damage.DamageType.Physical, stacks > 1 ? basicDamage + addidentionalDamage * (stacks - 1) : basicDamage);
+            //double realDamage = ObjectManager.Player.CalcDamage(target, Damage.DamageType.Physical, stacks > 1 ? basicDamage + (basicDamage * extraDamage) * (stacks - 1) : basicDamage);
             if (Kalista.debug)
             {
-                Game.PrintChat("Target: " + target.SkinName + " Total to target: " + (int)realDamage + " || Dealing " + basicDamagex + "(+" + (int)(0.6 * (ObjectManager.Player.BaseAttackDamage + ObjectManager.Player.FlatPhysicalDamageMod)) + ") Will do: " + (int)basicDamage + (basicDamage * extraDamage)*(stacks-1) + " (" + stacks + ")");
+                Game.PrintChat("Target: " + target.SkinName + " Total to target: " + (int)realDamage + " || Dealing " + basicDamagex + "(+" + (int)(0.6 * (ObjectManager.Player.BaseAttackDamage + ObjectManager.Player.FlatPhysicalDamageMod)) + ") Will do: " + (int)basicDamage + addidentionalDamage * (stacks - 1) + " (" + stacks + ")");
+//                Game.PrintChat("Target: " + target.SkinName + " Total to target: " + (int)realDamage + " || Dealing " + basicDamagex + "(+" + (int)(0.6 * (ObjectManager.Player.BaseAttackDamage + ObjectManager.Player.FlatPhysicalDamageMod)) + ") Will do: " + (int)basicDamage + (basicDamage * extraDamage) * (stacks - 1) + " (" + stacks + ")");
             }
             return realDamage;
         }
