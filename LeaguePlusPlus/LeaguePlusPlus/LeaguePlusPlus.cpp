9e9b38d79be6e3d79138f2f380780e51127e3d74
TwilightLoL <info@team-xte.com>
2014-12-04 22:35:20 +0200

    L++

---
+++ LeaguePlusPlus/LeaguePlusPlus/LeaguePlusPlus.cpp
@@ -0,0 +1,12 @@
+// LeaguePlusPlus.cpp : main project file.
+
+#include "stdafx.h"
+#using "LeagueSharp.dll"
+#using "LeagueSharp.Common.dll"
+
+using namespace System;
+
+void main()
+{
+	LeagueSharp::Game::PrintChat("Hi world");
+}
