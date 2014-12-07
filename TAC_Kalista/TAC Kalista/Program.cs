0f6587b6fbb392dbf39716bbd606de64f9b6d715
TwilightLoL <info@team-xte.com>
2014-12-07 09:59:54 +0200

    Revert "v3.8"
    This reverts commit c9a6fbdfce36505b29b8d1ead027351f3758711f.

--- TAC_Kalista/TAC Kalista/Program.cs
+++ TAC_Kalista/TAC Kalista/Program.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using LeagueSharp;
 using LeagueSharp.Common;
-using SharpDX;
 
 namespace TAC_Kalista
 {
@@ -18,28 +17,19 @@ namespace TAC_Kalista
         static void Main(string[] args)
         {
             Game.PrintChat("---------------------------");
-            Game.PrintChat("[<font color='#FF0000'>v3.8</font>]<font color='#7A6EFF'>Twilight's Auto Carry:</font> <font color='#86E5E1'>Kalista</font>");
+            Game.PrintChat("[<font color='#FF0000'>v3.7</font>]<font color='#7A6EFF'>Twilight's Auto Carry:</font> <font color='#86E5E1'>Kalista</font>");
             CustomEvents.Game.OnGameLoad += Load;
         }
         public static void Load(EventArgs args)
         {
-            if (ObjectManager.Player.ChampionName == "Kalista")
-            {
-                SkillHandler.init();
-                ItemHandler.init();
-                MenuHandler.init();
-                DrawingHandler.init();
-                Game.OnGameUpdate += OnGameUpdateModes;
-                AntiGapcloser.OnEnemyGapcloser += FightHandler.AntiGapCloser;
-                Obj_AI_Hero.OnProcessSpellCast += FightHandler.OnProcessSpellCast;
-            }
-            else if (KalistaInGame() && ObjectManager.Player.ChampionName != "Kalista")
-            {
-                SkillHandler.initOath();
-                MenuHandler.initOath();
-                Game.OnGameProcessPacket += Game_OnGameProcessPacket;
-                Game.OnGameUpdate += Game_OnGameUpdateOath;
-            }
+            if (ObjectManager.Player.ChampionName != "Kalista") return;
+            SkillHandler.init();
+            ItemHandler.init();
+            MenuHandler.init();
+            DrawingHandler.init();
+            Game.OnGameUpdate += OnGameUpdateModes;
+            AntiGapcloser.OnEnemyGapcloser += FightHandler.AntiGapCloser;
+            Obj_AI_Hero.OnProcessSpellCast += FightHandler.OnProcessSpellCast;
         }
         public static void OnGameUpdateModes(EventArgs args)
         {
@@ -66,61 +56,6 @@ namespace TAC_Kalista
             }
             SmiteHandler.Init();
 
-        }
-        /**
-         * @author Trees
-         * */
-        internal static void Game_OnGameUpdateOath(EventArgs args)
-        {
-            if (KalistaInGame() && ObjectManager.Player.ChampionName != "Kalista")
-            {
-                if (!MenuHandler.Config.Item("Enabled").GetValue<KeyBind>().Active ||
-                    ObjectManager.Player.HealthPercentage() < MenuHandler.Config.Item("Health").GetValue<Slider>().Value)
-                {
-                    return;
-                }
-
-                if (SkillHandler.Oathsworn.Instance == null || SkillHandler.Oathsworn.Instance.Name == null || SkillHandler.Oathsworn.Instance.Name == "BaseSpell" ||
-                    (SkillHandler.Oathsworn.Instance.Name == "KalistaRAllyDash" && SkillHandler.State != 3))
-                {
-                    return;
-                }
-
-                SkillHandler.SetState();
-
-                if (SkillHandler.State != 0)
-                {
-                    return;
-                }
-
-                Obj_AI_Hero targ = SimpleTs.GetTarget(350, SimpleTs.DamageType.Magical);
-                Vector3 pos = targ.IsValidTarget() ? targ.ServerPosition : Game.CursorPos;
-                Packet.C2S.Cast.Struct p = new Packet.C2S.Cast.Struct(0, SkillHandler.Oathsworn.Slot, -1, pos.X, pos.Y, pos.X, pos.Y, 0xF2);
-                Packet.C2S.Cast.Encoded(p).Send();
-            }
-        }
-        /**
-         * @author Trees
-         * */
-        private static bool KalistaInGame()
-        {
-            return ObjectManager.Get<Obj_AI_Hero>().Any(hero => hero.IsAlly && hero.ChampionName == "Kalista");
-        }
-        /**
-         * @author Trees
-         * */
-        private static void Game_OnGameProcessPacket(GamePacketEventArgs args)
-        {
-            if (KalistaInGame() && ObjectManager.Player.ChampionName != "Kalista")
-            {
-                if (args.PacketData[0] != Packet.MultiPacket.Header ||
-                    args.PacketData[5] != (byte)Packet.MultiPacketType.LockCamera ||
-                    !MenuHandler.Config.Item("BlockCamera").GetValue<bool>())
-                {
-                    return;
-                }
-                args.Process = false;
-            }
         }
     }
 }
--- TAC_Kalista/TAC Kalista/SkillHandler.cs
+++ TAC_Kalista/TAC Kalista/SkillHandler.cs
@@ -11,46 +11,15 @@ namespace TAC_Kalista
 {
     class SkillHandler
     {
-        internal static Spell Q, W, E, R, Oathsworn;
-        internal static Spell[] spellList = { Q, W, E, R };
-        internal static int State = 0;
-        internal static void init()
+        public static Spell Q, W, E, R;
+        public static Spell[] spellList = { Q, W, E, R };
+        public static void init()
         {
             Q = new Spell(SpellSlot.Q, 1450);
             W = new Spell(SpellSlot.W, 5500);
             E = new Spell(SpellSlot.E, 1000);
             R = new Spell(SpellSlot.R, 1200);
             Q.SetSkillshot(0.25f, 60f, 2000f, true, SkillshotType.SkillshotLine);
-        }
-        /**
-         * @author Trees
-         * */
-        internal static void initOath()
-        {
-            Oathsworn = new Spell((SpellSlot)0x3C, 300);
-            Oathsworn.SetSkillshot(.862f, 20, float.MaxValue, false, SkillshotType.SkillshotLine);
-        }
-
-        /**
-         * @author Trees
-         * */
-        internal static void SetState()
-        {
-            switch (Oathsworn.Instance.Name)
-            {
-                case "KalistaRAllyDashCantCast1":
-                    State = 1;
-                    break;
-                case "KalistaRAllyDashCantCast2":
-                    State = 2;
-                    break;
-                case "KalistaRAllyDashCantCast3":
-                    State = 3;
-                    break;
-                case "KalistaRAllyDash":
-                    State = 0;
-                    break;
-            }
         }
     }
 }
